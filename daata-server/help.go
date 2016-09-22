739fffb038f8a025a51f7de445469a6710055c1a
Sairam Kunala <sairam@kunala.com>
2016-09-22 21:50:19 +0530

    move to template

--- daata-server/help.go
+++ daata-server/help.go
@@ -10,16 +10,8 @@ type Data struct {
 	URL string
 }
 
-const helpInfo = `
-curl -i -X POST {{.URL}}/upload -H "Content-Type: application/zip" --data-binary "@data.zip"
-curl -i -X POST {{.URL}}/upload -H "Content-Type: application/json" --data-binary "@freshmenu.json"
-
-Examples:
-{{.URL}}/d/wwpbbi
-{{.URL}}/d/iwhspu
-`
-
 func help(w io.Writer) {
-	t, _ := template.New("").Parse(helpInfo)
+	filename := "usage.tmpl"
+	t, _ := template.New(filename).ParseFiles("tmpl/" + filename)
 	t.Execute(w, Data{URL: serverURL})
 }
